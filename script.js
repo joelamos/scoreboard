3f5a9332938f55012f4b9035af1fe2560a589ef7
Joel Christophel <joel@joelchristophel.com>
2018-05-15 20:39:13 -0400

    Better indentation

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 function incrementScore(element) {
-  var currentScore = parseInt(element.innerHTML);
-  element.innerHTML = currentScore + 1;
+    var currentScore = parseInt(element.innerHTML);
+    element.innerHTML = currentScore + 1;
 }
 
 function reset() {
-  var scores = $('.score').each(function() {
-    this.innerHTML = 0;
-  });
+    var scores = $('.score').each(function () {
+        this.innerHTML = 0;
+    });
 }
 
-$(function() {
-  $('.score').click(function() {
-    incrementScore(this);
-  });
-  $('#reset').click(function() {
-    reset();
-  });
+$(function () {
+    $('.score').click(function () {
+        incrementScore(this);
+    });
+    $('#reset').click(function () {
+        reset();
+    });
 });
