6b650f868b4a645c12db16bc3acb64bec3dce264
Joel Christophel <joel@joelchristophel.com>
2018-08-30 00:08:08 -0400

    Add ability to record games
    Write games to a Cloud Firestore database when two players have been specfied in the player dropdown menus

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ function updateScore($scoreElement, increment) {
         var deuceMode = totalScore >= maxScore * 2 - 1;
         if ((deuceMode && score - otherScore > 1) || (!deuceMode && score == maxScore)) {
             $scoreElement.addClass('text-success');
+            recordGame();
             return;
         }
         var serves = maxScore == 21 ? 5 : 2;
@@ -29,9 +30,33 @@ function switchServer() {
 function reset() {
     var scores = $('.score').removeClass('text-success').text('0');
     $('#game-to-21 input').bootstrapToggle('enable');
+    $('.player-dropdown').val('');
+}
+
+function recordGame() {
+    if ($('.player-dropdown')[0].value && $('.player-dropdown')[1].value) {
+        db.collection('games').add({
+            player1: db.doc('users/' + $('.player-dropdown').eq(0).val()),
+            player1Score: parseInt($('.score').eq(0).text()),
+            player2: db.doc('users/' + $('.player-dropdown').eq(1).val()),
+            player2Score: parseInt($('.score').eq(1).text()),
+            time: new Date()
+        })
+    }
+}
+
+function populatePlayerDropdowns() {
+    var $dropdowns = $('.player-dropdown');
+    $dropdowns.append('<option></option>');
+    db.collection('users').orderBy('displayName').get().then((snapshot) => {
+        snapshot.forEach((user) => {
+            $dropdowns.append('<option value="' + `${user.id}` + '">' + user.data().displayName + '</option>');
+        });
+    });
 }
 
 $(function () {
+    populatePlayerDropdowns();
     $('.score-container').click(function () {
         updateScore($(this).find('.score'), true);
     });
